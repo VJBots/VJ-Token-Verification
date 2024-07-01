bc43e310b016830fc6730fb24cc7f00e71e4b394
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-01 11:08:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,3 +3,5 @@ from os import environ
 API = environ.get("API", "") # shortlink api
 URL = environ.get("URL", "") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
